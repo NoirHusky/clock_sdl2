38d63f86e88b8c20eb2e977790f77f6acb802cb7
NoirHusky <samuelverev@gmail.com>
2023-08-14 17:38:34 +0100

    draw clock

--- clock.c
+++ clock.c
@@ -11,33 +11,6 @@ SDL_Window *g_window;
 SDL_Renderer *g_renderer;
 bool g_running = false;
 
-/* struct tvector { */
-/*     SDL_Point** data; */
-/*     int len; */
-/*     int cap; */
-/* }; */
-/* typedef struct tvector vector; */
-
-/* vector* vector_new(int size) { */
-/*     vector* v = malloc(sizeof(vector)); */
-/*     v->data = malloc(sizeof(SDL_Point*) * size); */
-/*     v->len = 0; */
-/*     v->cap = size; */
-/*     return v; */
-/* } */
-/* void vector_resize(vector* v) { } */
-/* void vector_pushback(vector* v, SDL_Point* data) { */
-/*     if ( v->len > v->cap ) { */
-/*         vector_resize(v); */
-/*     } */
-/*     v->data[v->len] = data; */
-/*     v->len++; */
-/* } */
-
-/* SDL_Point* vector_at(vector* v, int at) { */
-/*     return v->data[at]; */
-/* } */
-
 SDL_FPoint vector_rotate(float x, float y, float angle) {
     SDL_FPoint p;
     float theta = angle * (M_PI / 180.0);
@@ -50,24 +23,252 @@ SDL_FPoint vector_rotate(float x, float y, float angle) {
     return p;
 }
 
-void draw_clock() {
-    /* SDL_Point points[20]; */
+int pixel(SDL_Renderer *renderer, Sint16 x, Sint16 y) {
+    return SDL_RenderDrawPoint(renderer, x, y);
+}
+int hline(SDL_Renderer *renderer, Sint16 x1, Sint16 x2, Sint16 y) {
+    return SDL_RenderDrawLine(renderer, x1, y, x2, y);
+    ;
+}
+int vline(SDL_Renderer *renderer, Sint16 x, Sint16 y1, Sint16 y2) {
+    return SDL_RenderDrawLine(renderer, x, y1, x, y2);
+    ;
+}
 
-    /* for ( int i = 0; i < 20; i++ ) { */
-    /*     SDL_FPoint p = vector_rotate(0, 1, 10*i); */
-    /*     points[i].x = p.x * 100; */
-    /*     points[i].y = p.y * 100; */
-    /* } */
-    /* SDL_RenderDrawLines(g_renderer, points, 19); */
+int _drawQuadrants(SDL_Renderer *renderer, Sint16 x, Sint16 y, Sint16 dx,
+                   Sint16 dy, Sint32 f) {
+    int result = 0;
+    Sint16 xpdx, xmdx;
+    Sint16 ypdy, ymdy;
+
+    if (dx == 0) {
+        if (dy == 0) {
+            result |= pixel(renderer, x, y);
+        } else {
+            ypdy = y + dy;
+            ymdy = y - dy;
+            if (f) {
+                result |= vline(renderer, x, ymdy, ypdy);
+            } else {
+                result |= pixel(renderer, x, ypdy);
+                result |= pixel(renderer, x, ymdy);
+            }
+        }
+    } else {
+        xpdx = x + dx;
+        xmdx = x - dx;
+        ypdy = y + dy;
+        ymdy = y - dy;
+        if (f) {
+            result |= vline(renderer, xpdx, ymdy, ypdy);
+            result |= vline(renderer, xmdx, ymdy, ypdy);
+        } else {
+            result |= pixel(renderer, xpdx, ypdy);
+            result |= pixel(renderer, xmdx, ypdy);
+            result |= pixel(renderer, xpdx, ymdy);
+            result |= pixel(renderer, xmdx, ymdy);
+        }
+    }
+
+    return result;
+}
+
+#define ELLIPSE_OVERSCAN 4
+int _ellipseRGBA(SDL_Renderer *renderer, Sint16 x, Sint16 y, Sint16 rx,
+                 Sint16 ry, Uint8 r, Uint8 g, Uint8 b, Uint8 a, Sint32 f) {
+    int result;
+    Sint32 rx2, ry2, rx22, ry22;
+    Sint32 error;
+    Sint32 curX, curY, curXp1, curYm1;
+    Sint32 scrX, scrY, oldX, oldY;
+    Sint32 deltaX, deltaY;
+
+    /*
+     * Sanity check radii
+     */
+    if ((rx < 0) || (ry < 0)) {
+        return (-1);
+    }
+
+    /*
+     * Set color
+     */
+    result = 0;
+    result |= SDL_SetRenderDrawBlendMode(
+        renderer, (a == 255) ? SDL_BLENDMODE_NONE : SDL_BLENDMODE_BLEND);
+    result |= SDL_SetRenderDrawColor(renderer, r, g, b, a);
+
+    /*
+     * Special cases for rx=0 and/or ry=0: draw a hline/vline/pixel
+     */
+    if (rx == 0) {
+        if (ry == 0) {
+            return (pixel(renderer, x, y));
+        } else {
+            return (vline(renderer, x, y - ry, y + ry));
+        }
+    } else {
+        if (ry == 0) {
+            return (hline(renderer, x - rx, x + rx, y));
+        }
+    }
+
+    /*
+     * Top/bottom center points.
+     */
+    oldX = scrX = 0;
+    oldY = scrY = ry;
+    result |= _drawQuadrants(renderer, x, y, 0, ry, f);
+
+    /* Midpoint ellipse algorithm with overdraw */
+    rx *= ELLIPSE_OVERSCAN;
+    ry *= ELLIPSE_OVERSCAN;
+    rx2 = rx * rx;
+    rx22 = rx2 + rx2;
+    ry2 = ry * ry;
+    ry22 = ry2 + ry2;
+    curX = 0;
+    curY = ry;
+    deltaX = 0;
+    deltaY = rx22 * curY;
+
+    /* Points in segment 1 */
+    error = ry2 - rx2 * ry + rx2 / 4;
+    while (deltaX <= deltaY) {
+        curX++;
+        deltaX += ry22;
+
+        error += deltaX + ry2;
+        if (error >= 0) {
+            curY--;
+            deltaY -= rx22;
+            error -= deltaY;
+        }
+
+        scrX = curX / ELLIPSE_OVERSCAN;
+        scrY = curY / ELLIPSE_OVERSCAN;
+        if ((scrX != oldX && scrY == oldY) || (scrX != oldX && scrY != oldY)) {
+            result |= _drawQuadrants(renderer, x, y, scrX, scrY, f);
+            oldX = scrX;
+            oldY = scrY;
+        }
+    }
+
+    /* Points in segment 2 */
+    if (curY > 0) {
+        curXp1 = curX + 1;
+        curYm1 = curY - 1;
+        error = ry2 * curX * curXp1 + ((ry2 + 3) / 4) + rx2 * curYm1 * curYm1 -
+                rx2 * ry2;
+        while (curY > 0) {
+            curY--;
+            deltaY -= rx22;
+
+            error += rx2;
+            error -= deltaY;
+
+            if (error <= 0) {
+                curX++;
+                deltaX += ry22;
+                error += deltaX;
+            }
+
+            scrX = curX / ELLIPSE_OVERSCAN;
+            scrY = curY / ELLIPSE_OVERSCAN;
+            if ((scrX != oldX && scrY == oldY) ||
+                (scrX != oldX && scrY != oldY)) {
+                oldY--;
+                for (; oldY >= scrY; oldY--) {
+                    result |= _drawQuadrants(renderer, x, y, scrX, oldY, f);
+                    /* prevent overdraw */
+                    if (f) {
+                        oldY = scrY - 1;
+                    }
+                }
+                oldX = scrX;
+                oldY = scrY;
+            }
+        }
+
+        /* Remaining points in vertical */
+        if (!f) {
+            oldY--;
+            for (; oldY >= 0; oldY--) {
+                result |= _drawQuadrants(renderer, x, y, scrX, oldY, f);
+            }
+        }
+    }
+
+    return (result);
+}
+
+int filledCircleRGBA(SDL_Renderer *renderer, Sint16 x, Sint16 y, Sint16 rad,
+                     Uint8 r, Uint8 g, Uint8 b, Uint8 a) {
+    return _ellipseRGBA(renderer, x, y, rad, rad, r, g, b, a, 1);
+}
+
+void draw_clock() {
+    // draw the circle
+    filledCircleRGBA(g_renderer, WIDTH / 2, HEIGHT / 2, WIDTH / 2.f * 0.9f, 255,
+                     255, 255, 255);
+    filledCircleRGBA(g_renderer, WIDTH / 2, HEIGHT / 2, WIDTH / 2.f * 0.1f, 0,
+                     0, 0, 255);
+    // draw the piecies left and right and top and bottom
+    SDL_Rect r_top, r_bot, r_rgt, r_lft;
+    r_top.w = 10;
+    r_top.h = 50;
+    r_top.x = WIDTH / 2 - r_top.w / 2;
+    r_top.y = 10;
+    SDL_RenderDrawRect(g_renderer, &r_top);
+
+    r_bot.w = 10;
+    r_bot.h = 50;
+    r_bot.x = WIDTH / 2 - r_bot.w / 2;
+    r_bot.y = HEIGHT - 10 - r_bot.h;
+    SDL_RenderDrawRect(g_renderer, &r_bot);
+
+    r_rgt.w = 50;
+    r_rgt.h = 10;
+    r_rgt.x = WIDTH - r_rgt.w - 10;
+    r_rgt.y = HEIGHT / 2;
+    SDL_RenderDrawRect(g_renderer, &r_rgt);
+
+    r_lft.w = 50;
+    r_lft.h = 10;
+    r_lft.x = 10;
+    r_lft.y = HEIGHT / 2;
+    SDL_RenderDrawRect(g_renderer, &r_lft);
+
+    // draw the hands of the clock
+    SDL_Rect sec_hand;
+    sec_hand.w = 10;
+    sec_hand.h = 70;
+    sec_hand.x = WIDTH / 2 - sec_hand.w / 2;
+    sec_hand.y = HEIGHT / 2 - (sec_hand.h + 50);
+    SDL_RenderDrawRect(g_renderer, &sec_hand);
+
+    SDL_Rect min_hand;
+    min_hand.w = 10;
+    min_hand.h = 70;
+    min_hand.x = WIDTH / 2 - min_hand.w / 2;
+    min_hand.y = HEIGHT / 2 - (min_hand.h + 50);
+    SDL_RenderFillRect(g_renderer, &min_hand);
+
+    SDL_Rect hour_hand;
+    hour_hand.w = 10;
+    hour_hand.h = 50;
+    hour_hand.x = WIDTH / 2 - hour_hand.w / 2;
+    hour_hand.y = HEIGHT / 2 - (hour_hand.h + 50);
+    SDL_RenderDrawRect(g_renderer, &hour_hand);
 }
 
 void update(float dt) {}
 
 void render() {
-    SDL_SetRenderDrawColor(g_renderer, 255, 150, 150, 255);
+    SDL_SetRenderDrawColor(g_renderer, 0, 0, 0, 255);
     SDL_RenderClear(g_renderer);
-    // TODO: do your logic here
-    /* draw_clock(); */
+    // TODO:
+    draw_clock();
     SDL_RenderPresent(g_renderer);
 }
 
